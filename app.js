75bab5d948f62e3aab235f9c05300b5a4ff11331
Javeria Masood <javeriamails@gmail.com>
2025-03-03 15:36:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut,signInWithPopup,GoogleAuthProvider, onAuthStateChanged,} from "https://www.gstatic.com/firebasejs/11.4.0/firebaseauth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyBvhp5RdY17DSQCO6p6vLrQ5E2RasBH-IM",
+    authDomain: "sign-up-login-form-f52a3.firebaseapp.com",
+    projectId: "sign-up-login-form-f52a3",
+    storageBucket: "sign-up-login-form-f52a3.firebasestorage.app",
+    messagingSenderId: "339294474621",
+    appId: "1:339294474621:web:f40c5c517c66c544effc14",
+    measurementId: "G-J2985QSGLZ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const auth=getAuth(app);
+
+   // Sign Up with Email/Password 
+  document.getElementById('signup-btn')?.addEventListener('click', (e)=>{
+  e.preventDefault();
+  let email=document.getElementById('signup-email').value ;
+  let password =document.getElementById('signup-password').value ;
+  createUserWithEmailAndPassword(auth,email,password)
+  .then(()=>{
+    alert('"Sign Up Successful!');
+    window.location.href='welcome.html'
+  })
+  .catch((error)=>{
+   alert(error.message)
+})
+})
+
+
+// Login with Email/Password 
+document.getElementById('login-btn')?.addEventListener('click',(e)=>{
+e.preventDefault();
+let email=document.getElementById('login-email').value ;
+let password =document.getElementById('login-password').value ;
+signInWithEmailAndPassword(auth,email,password)
+.then(()=>{
+alert('"Login Successful!')
+window.location.href = "welcome.html"; 
+    })
+.catch((error) => {       
+    alert(error.message);     
+    }); 
+
+})
+
+// Continue with Google 
+document.getElementById("google-btn")?.addEventListener("click", () => { 
+signInWithPopup(auth, provider) 
+.then(() => { 
+    alert("Login Successful!"); 
+    window.location.href = "welcome.html"; 
+}) 
+.catch((error) => {       
+    alert(error.message);     
+}); 
+});  
+
+//Logout
+document.getElementById("logout-btn")?.addEventListener("click", () => { 
+    signOut(auth)     
+    .then(() => { 
+        alert("Logged Out Successfully!");      
+         window.location.href = "index.html"; 
+        })     
+        .catch((error) => {       
+            alert(error.message);     
+        });
+     });  
+     // Show User Email on Welcome Page 
+     onAuthStateChanged(auth, (user) => { 
+        if (user && window.location.pathname.includes("welcome.html")) { 
+            document.getElementById("user-email").textContent = user.email; 
+        } else if (!user && window.location.pathname.includes("welcome.html")) { 
+            window.location.href = "index.html";  
+         }
+         }); 
