386fb39e4857b7d474d86e460a8b41197f937fbe
Javeria Masood <javeriamails@gmail.com>
2025-03-08 14:57:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -16,67 +16,66 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
   const auth=getAuth(app);
+  const provider=new GoogleAuthProvider();
 
-   // Sign Up with Email/Password 
-  document.getElementById('signup-btn')?.addEventListener('click', (e)=>{
-  e.preventDefault();
-  let email=document.getElementById('signup-email').value ;
-  let password =document.getElementById('signup-password').value ;
-  createUserWithEmailAndPassword(auth,email,password)
-  .then(()=>{
-    alert('"Sign Up Successful!');
-    window.location.href='welcome.html'
-  })
-  .catch((error)=>{
-   alert(error.message)
-})
-})
-
-
-// Login with Email/Password 
-document.getElementById('login-btn')?.addEventListener('click',(e)=>{
-e.preventDefault();
-let email=document.getElementById('login-email').value ;
-let password =document.getElementById('login-password').value ;
-signInWithEmailAndPassword(auth,email,password)
-.then(()=>{
-alert('"Login Successful!')
-window.location.href = "welcome.html"; 
+  // Sign Up with Email/Password
+  document.getElementById("signup-btn")?.addEventListener("click", () => {
+    const email = document.getElementById("signup-email").value;
+    const password = document.getElementById("signup-password").value;
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+    alert("Sign Up Successful!");
+    window.location.href = "welcome.html";
     })
-.catch((error) => {       
-    alert(error.message);     
-    }); 
+    .catch((error) => {
+    alert(error.message);
+    });
+    });
 
-})
-
-// Continue with Google 
-document.getElementById("google-btn")?.addEventListener("click", () => { 
-signInWithPopup(auth, provider) 
-.then(() => { 
-    alert("Login Successful!"); 
-    window.location.href = "welcome.html"; 
-}) 
-.catch((error) => {       
-    alert(error.message);     
-}); 
-});  
-
-//Logout
-document.getElementById("logout-btn")?.addEventListener("click", () => { 
-    signOut(auth)     
-    .then(() => { 
-        alert("Logged Out Successfully!");      
-         window.location.href = "index.html"; 
-        })     
-        .catch((error) => {       
-            alert(error.message);     
+     // Login with Email/Password
+     document.getElementById("login-btn")?.addEventListener("click", () => {
+        const email = document.getElementById("login-email").value;
+        const password = document.getElementById("login-password").value;
+        signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+        alert("Login Successful!");
+        window.location.href = "welcome.html";
+        })
+        .catch((error) => {
+        alert(error.message);
         });
-     });  
-     // Show User Email on Welcome Page 
-     onAuthStateChanged(auth, (user) => { 
-        if (user && window.location.pathname.includes("welcome.html")) { 
-            document.getElementById("user-email").textContent = user.email; 
-        } else if (!user && window.location.pathname.includes("welcome.html")) { 
-            window.location.href = "index.html";  
-         }
-         }); 
+        });
+
+    // Continue with Google
+       
+    document.getElementById("google-btn")?.addEventListen("click", () => {
+    signInWithPopup(auth, provider)
+    .then(() => {
+    alert("Login Successful!");
+    window.location.href = "welcome.html";
+    })
+    .catch((error) => {
+    alert(error.message);
+    });
+    });
+
+// Logout
+document.getElementById("logout-btn")?.addEventListener("click", () => {
+    signOut(auth)
+    .then(() => {
+    alert("Logged Out Successfully!");
+    window.location.href = "index.html";
+    })
+    .catch((error) => {
+    alert(error.message);
+    });
+    });
+
+   // Show User Email on Welcome Page
+onAuthStateChanged(auth, (user) => {
+    if (user && window.location.pathname.includes("welcome.html")) {
+    document.getElementById("user-email").textContent = user.email;
+    } else if (!user && window.location.pathname.includes("welcome.html")) {
+    window.location.href = "index.html";
+    }
+    });
